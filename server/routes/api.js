93df408f4be09b2ab2368631195c45a784ef574a
Satheeshrajulapati <satheesh.rajulapati@gmail.com>
2025-07-05 23:56:00 +0530

    mongoose install and db connection setup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb+srv://MEAN:MEAN@cluster0.fufnnxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
+
+mongoose.connect(dbURI)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 router.get('/', (req, res) => {
     res.send('Hello, API is running!');
-})
+});
 
 module.exports = router;
